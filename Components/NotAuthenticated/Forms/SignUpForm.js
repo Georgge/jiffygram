fc6d293f34d2c08511e6aadd539adc71c8fc2f6e
= <jo123.garcia.s@gmail.com>
2018-07-24 21:09:46 -0500

    reducer as form

--- Components/NotAuthenticated/Forms/SignUpForm.js
+++ Components/NotAuthenticated/Forms/SignUpForm.js
@@ -1,21 +1,32 @@
 import React, {Component} from 'react';
-import {View, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
 
 import {Field, reduxForm} from 'redux-form';
 
-const fieldName = () => {
+const fieldName = (props) => {
   return (
     <TextInput
       placeholder="From fieldName"
+      onChangeText={props.input.onChange}
+      value={props.input.value}
     />
   );
 };
 
-const SignUpForm = () => {
+const SignUpForm = (props) => {
+  console.log(props);
   return (
     <View>
       <Field name="name" component={fieldName} />
       <Text>Redux Form</Text>
+      <Button
+        title="Register"
+        onPress={
+          props.handleSubmit((values) => {
+            console.log(values);
+          })
+        }
+      />
     </View>
   );
 };
--- Store/Store.js
+++ Store/Store.js
@@ -1,9 +1,11 @@
 import {createStore, combineReducers} from 'redux';
+import {reducer as form} from 'redux-form';
 
 const reducerTest = (state=[0], action) => state;
 
 const reducers = combineReducers({
   reducerTest,
+  form,
 });
 
 const store = createStore(reducers);
