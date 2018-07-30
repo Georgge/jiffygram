e73cb489774ce2d7f4088bc6e6c07f69d48acedd
= <jo123.garcia.s@gmail.com>
2018-07-29 21:47:11 -0500

    firebase key

--- Store/Services/Firebase.js
+++ Store/Services/Firebase.js
@@ -1,14 +1,18 @@
 import * as firebase from 'firebase';
+import FirebaseKey from './FirebaseConfig';
+
 
 // Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyCHgZsXhSCQm6cqI6LGhVIsiwX773eQoyE',
-  authDomain: 'jiffygram-805ea.firebaseapp.com',
-  databaseURL: 'https://jiffygram-805ea.firebaseio.com',
-  projectId: 'jiffygram-805ea',
-  storageBucket: 'jiffygram-805ea.appspot.com',
-  messagingSenderId: '990165322036',
-};
-firebase.initializeApp(config);
+/* onst config = {
+** apiKey: '',
+** authDomain: '',
+** databaseURL: '',
+** projectId: '',
+** orageBucket: '',
+** messagingSenderId: '',
+** };
+**/
+
+firebase.initializeApp(FirebaseKey);
 
 export const authentication = firebase.auth();
