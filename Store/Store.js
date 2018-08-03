22a52fe767c05b7de323fe1d1eb9c8861ab0fea1
= <jo123.garcia.s@gmail.com>
2018-08-03 14:15:55 -0500

    initializing redux saga

--- Store/Store.js
+++ Store/Store.js
@@ -1,13 +1,23 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {reducer as form} from 'redux-form';
+import createSagaMiddleware from 'redux-saga';
 
 const reducerTest = (state=[0], action) => state;
+
+function* primaryFunction() {
+  // yield ES6
+  console.log('from generated fuction');
+}
+
+const sagaMiddleware = createSagaMiddleware();
 
 const reducers = combineReducers({
   reducerTest,
   form,
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(primaryFunction);
 
 export default store;
